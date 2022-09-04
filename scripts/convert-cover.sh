a3f4f4511558d8b042544623ec0c03e9c81a107a
Feng Kaiyu <loveress01@outlook.com>
2022-09-04 20:08:24 +0800

    chore: update cover converter script

--- scripts/convert-cover.sh
+++ scripts/convert-cover.sh
@@ -1,6 +1,12 @@
-# This script convert all PDF covers to PNG format.
+#!/bin/bash
 
-ls ../*/main.pdf | sed -r 's|/[^/]+$||' | sed -r 's|^../||' | sort | uniq | xargs -I {} convert -density 300 "../{}/main.pdf[0]" -quality 100 "{}.png"
+# cd to the directory where the script is located
+cd "$(dirname "$0")" || exit
+
+mkdir -p templates
+
+# This script convert all PDF covers to PNG format.
+find ../templates/ -type f -name 'main.pdf' | sed -r 's|/[^/]+$||' | sed -r 's|^../||' | sort | uniq | xargs -I {} convert -density 300 "../{}/main.pdf[0]" -quality 100 "{}.png"
 
 # Special case for `master-thesis` as second page is more interesting.
-convert -density 300 "../master-thesis/main.pdf[1]" -quality 100 "master-thesis.png"
+convert -density 300 "../templates/graduate-thesis/main.pdf[2]" -quality 100 "templates/graduate-thesis.png"
